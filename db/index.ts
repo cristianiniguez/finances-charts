5499186ab3f68991ef5882b70845d2edffa96eaa
cristianiniguez <felizpc1@gmail.com>
2024-06-14 23:08:02 +0000

    fixed getAccountData

--- db/index.ts
+++ db/index.ts
@@ -14,24 +14,24 @@ export const getAccountData = async (
 
   const data = results.filter(isFullPage).map(({ properties }) => ({
     detail: (({ detail: detailRaw }) => {
-      if (detailRaw.type !== 'title') return ''
+      if (!detailRaw || detailRaw.type !== 'title') return ''
 
       return detailRaw.title.map(({ plain_text }) => plain_text).join('')
     })(properties),
 
     date: (({ date: dateRaw }) => {
-      if (dateRaw.type !== 'date') return ''
+      if (!dateRaw || dateRaw.type !== 'date') return ''
       return dateRaw.date?.start ?? ''
     })(properties),
 
     income: (({ income: incomeRaw }) =>
-      (incomeRaw.type === 'number' && incomeRaw.number) || 0)(properties),
+      (incomeRaw && incomeRaw.type === 'number' && incomeRaw.number) || 0)(properties),
 
     outcome: (({ outcome: outcomeRaw }) =>
-      (outcomeRaw.type === 'number' && outcomeRaw.number) || 0)(properties),
+      (outcomeRaw && outcomeRaw.type === 'number' && outcomeRaw.number) || 0)(properties),
 
     tags: (({ tags: tagsRaw }) => {
-      if (tagsRaw.type !== 'multi_select') return []
+      if (!tagsRaw || tagsRaw.type !== 'multi_select') return []
       return tagsRaw.multi_select.map(opt => ({
         name: opt.name,
         color: opt.color
