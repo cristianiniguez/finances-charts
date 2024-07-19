561fa83890e40248196ef6c2f521d2a74a634681
cristianiniguez <felizpc1@gmail.com>
2024-07-19 00:37:41 +0000

    implemented app secret

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const url = new URL(request.url)
+  const secret = url.searchParams.get('s')
+
+  if (secret !== process.env.APP_SECRET) {
+    return NextResponse.json({ message: 'unauthorized '})
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/chart/:path*', '/kpi/:path*'],
+}
